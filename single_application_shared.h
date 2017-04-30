de59264710c912a3a136066fe3ab7593fadffaaf
Petr Semiletov <tea@list.ru>
2017-04-30 21:35:49 +0000

    more fixes to single application mode

---
+++ single_application_shared.h
@@ -0,0 +1,50 @@
+/*
+ * taken from http://berenger.eu/blog/c-qt-singleapplication-single-app-instance/
+ * code by Berenger Bramas
+ * modified by Peter Semiletov
+ * 
+ */
+
+#ifndef SINGLE_APP_SHARED_H
+#define SINGLE_APP_SHARED_H
+
+#include <QApplication>
+#include <QSharedMemory>
+#include <QStringList>
+
+#ifndef Q_OS_OS2
+#include <QSharedMemory>
+#endif
+
+
+
+#include <QLocalServer>
+
+class CSingleApplicationShared : public QApplication
+{
+        Q_OBJECT
+public:
+        CSingleApplicationShared (int &argc, char *argv[], const QString uniqueKey);
+
+        static int cursorFlashTime() {return 0;}; 
+        
+         bool alreadyExists() const{
+            return bAlreadyExists;
+        }
+        bool isMasterApp() const{
+            return !alreadyExists();
+        }
+
+        bool sendMessage(const QString &message);
+public slots:
+        void checkForMessage();
+ 
+signals:
+        void messageAvailable(const QStringList& messages);
+ 
+private:
+        bool bAlreadyExists;
+        QSharedMemory sharedMemory;
+};
+#endif // SINGLE_APP_SHARED_H
+

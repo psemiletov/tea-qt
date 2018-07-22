c28ccedb242c37c63a91dc32faf007753acd37ab
Petr Semiletov <tea@list.ru>
2018-07-22 19:53:27 +0300

    fix

--- libretta_calc.cpp
+++ libretta_calc.cpp
@@ -61,7 +61,9 @@ double calculate (string expression)
   if (end_pos != string::npos)
       do 
         {
-         for (size_t i = end_pos; i >= 0; i--)
+//         for (size_t i = end_pos; i >= 0; i--)
+         for (size_t i = end_pos; i-- > 0 ;)
+
              {
               if (expression[i] == '(')
                  {
